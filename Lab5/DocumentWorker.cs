15a1fe325c55b48484b7e13359e9c7542cd6f9f8
maxim <maxim@DESKTOP-MDIJHI5>
2018-03-13 16:08:57 +0200

    commit 1

---
+++ Lab5/DocumentWorker.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Word = Microsoft.Office.Interop.Word;
+using System.Reflection;
+
+namespace Lab5
+{
+    public class DocumentWorker
+    {
+        public static object template = "C:\\QuestionTemplate.dot";
+        Word.Application application;
+        Word.Document document;
+        Object missingObj = Missing.Value;
+        Object trueObj = true;
+        Object falseObj = false;
+        StreamReader sr;
+
+        public DocumentWorker(string path)
+        {
+            sr = File.OpenText(path);
+            application = new Word.Application();
+            object temp = template;
+            
+        }
+
+        public void Init()
+        {
+            try
+            {
+                document = application.Documents.Add(ref template, ref missingObj, ref missingObj, ref missingObj);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.ToString());
+                document.Close(ref falseObj, ref missingObj, ref missingObj);
+                application.Quit(ref missingObj, ref missingObj, ref missingObj);
+                document = null;
+                application = null;
+                throw error;
+            }
+            application.Visible = true;
+        }
+
+        public void SwapText(params string[] marks)
+        {
+            bool rangeFound;
+            Word.Range wordRange;
+            foreach (string mark in marks)
+            {
+                rangeFound = false;
+                for (int i = 1; i <= document.Sections.Count; i++)
+                {
+                    wordRange = document.Sections[i].Range;
+                    Word.Find wordFindObj = wordRange.Find;
+                    object[] wordFindParameters = new object[15]
+                    { mark, missingObj, missingObj, missingObj, missingObj, missingObj, missingObj,
+                    missingObj, missingObj, missingObj, missingObj, missingObj, missingObj, missingObj, missingObj };
+                    rangeFound = (bool)wordFindObj.GetType().InvokeMember
+                        ("Execute", BindingFlags.InvokeMethod, null, wordFindObj, wordFindParameters);
+                    if (rangeFound)
+                    {
+                        wordRange.Text = sr.ReadLine();
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void Save(string path, string fileName)
+        {
+            try
+            {
+                document.SaveAs(FileName: path + "\\" + fileName + ".docx");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+    }
+}
--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace Lab5
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Opening txt....");
+            DocumentWorker documentWorker = new DocumentWorker("C:\\EnglishTest.txt");
+
+            Console.WriteLine("Init Word document");
+            documentWorker.Init();
+
+            Console.WriteLine("Swapping marks to sentence");
+            documentWorker.SwapText("@@Q1", "@@a1", "@@b1", "@@c1", "@@Q2", "@@a2", "@@b2", "@@c2", "@@Q3",
+                "@@a3", "@@b3", "@@c3", "@@Q4", "@@a4", "@@b4", "@@c4", "@@Q5", "@@a5", "@@b5", "@@c5");
+
+            Console.WriteLine("Are you ready to save file Result.docx on D disk?");
+            Console.ReadKey();
+
+            documentWorker.Save("D:", "Result");
         }
     }
 }
